4df23c9bb7f1ba784f8739147817c28661c3783f
Danny Guinther <dannyguinther@gmail.com>
2014-11-27 23:53:32 -0500

    Update mavericks md5 sum

--- packer_templates/osx/mavericks.rb
+++ packer_templates/osx/mavericks.rb
@@ -1,6 +1,6 @@
 Racker::Processor.register_template do |mavericks|
   mavericks.variables = {
-    'iso_checksum' => '7db3997769e9f877dcaee30307ad4a15',
+    'iso_checksum' => '00a5420183525317dd3a16bbb25589a4',
     'iso_checksum_type' => 'md5',
     'iso_url' => 'packer_cache/OSX_InstallESD_10.9.5_13F34.dmg',
     'vm_name' => 'mavericks',
